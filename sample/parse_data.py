b671aa7b333c7d47182e7cc29ddb521847a59f22
Casper Bené <48201144+cappe04@users.noreply.github.com>
2022-10-26 18:49:11 +0200

    eminst letter reading

--- sample/parse_data.py
+++ sample/parse_data.py
@@ -4,13 +4,23 @@ from PIL import Image
 import numpy as np
 
 # https://www.nist.gov/itl/products-and-services/emnist-dataset
+
 __data_dir = os.path.join(os.path.dirname(__file__), "..", "data")
 
-emnist_data = loadmat(os.path.join(__data_dir, "EMNIST", "emnist-letters.mat"))
 
-arr = emnist_data["dataset"][0][0][0][0][0][0][327]
+if not os.path.exists(os.path.join(__data_dir, "EMNIST")):
+    raise Exception("Download the EMNIST dataset from Google Drive")
 
-arr = np.flip(np.rot90(arr.reshape((28, 28)), -1), 1)
+emnist_data = loadmat(os.path.join(__data_dir, "EMNIST", "emnist-letters.mat"))["dataset"][0][0][0][0][0]
 
+def get_data(data):
+    key = "abcdefghijklmnopqrstuvwxyz"
+    for i, img in enumerate(data[0]):
+        img_arr = np.flip(np.rot90(img.reshape((28, 28)), -1), 1)
+        yield key[data[1][i][0]-1], img_arr
 
-Image.fromarray(arr).show()  # ave("letter.png")
+i = 0
+for label, image in get_data(emnist_data):
+    if i > 10: break
+    Image.fromarray(image).save(f"image_tests/{label}.png")
+    i += 1
