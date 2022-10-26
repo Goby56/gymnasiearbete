03c59d984e1e02fc52a95232b9667a8727526733
Karl Rosengren <60710855+Goby56@users.noreply.github.com>
2022-10-26 14:09:41 +0200

    imported images

--- sample/parse_data.py
+++ sample/parse_data.py
@@ -1,8 +1,16 @@
 import os
 from scipy.io import loadmat
+from PIL import Image
+import numpy as np
 
 # https://www.nist.gov/itl/products-and-services/emnist-dataset
-__data_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'data'))
+__data_dir = os.path.join(os.path.dirname(__file__), "..", "data")
 
-loadmat(os.path.join(__data_dir, "EMNIST"))
+emnist_data = loadmat(os.path.join(__data_dir, "EMNIST", "emnist-letters.mat"))
 
+arr = emnist_data["dataset"][0][0][0][0][0][0][327]
+
+arr = np.flip(np.rot90(arr.reshape((28, 28)), -1), 1)
+
+
+Image.fromarray(arr).show()  # ave("letter.png")
