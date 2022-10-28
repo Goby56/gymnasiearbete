43a9ba1d2b16476e0f8bf49652a6864f1457e2a6
Casper Bené <48201144+cappe04@users.noreply.github.com>
2022-10-28 14:42:03 +0200

    added functions for saving wnb

--- sample/model.py
+++ sample/model.py
@@ -2,7 +2,7 @@ from contextlib import contextmanager
 from enum import Enum
 import os
 
-from model_file_formatter import file_formatter
+from model_file_formatter import file_reader, str_writer
 
 _MODEL_FOLDER = os.path.join(os.path.dirname( __file__ ), "..", "data\\models")
 
@@ -21,13 +21,19 @@ class Model(Enum):
         self._path = os.path.join(_MODEL_FOLDER, self._name_ + ".model")
         self.have_wnb = os.path.exists(self._path)
 
-    @contextmanager
     def load_wnb(self):
+        if not self._path: return None, None
         with open(self._path) as file:
-            yield file_formatter(file)
+            return file_reader(file)
+
+    def save_wnb(self, weights, biases):
+        with open(self._path, "w") as file:
+            formatted_str = str_writer(weights, biases)
+            file.write(formatted_str)
+
 
 if __name__ == "__main__":
     if Model.model1.have_wnb:
-        with Model.model1.load_wnb() as (weights, biases):
-            print(f"weights: {weights[0]}, biases: {biases[0]}")
-            
+        weights, biases = Model.model1.load_wnb()
+        print(f"weights: {weights[0]}, biases: {biases[0]}")
+        Model.model1.save_wnb(weights, biases)
--- sample/model_file_formatter.py
+++ sample/model_file_formatter.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def str_formatter(string: str):
+def str_reader(string: str):
     network = {
         "weights": [],
         "biases": []
@@ -15,11 +15,23 @@ def str_formatter(string: str):
             biases = []
             continue
         elements = line.split(";")[:-1]
-        weights.append([int(w) for w in elements[:-1]])
-        biases.append(elements[-1].split("(b)")[1])
+        weights.append([float(w) for w in elements[:-1]])
+        biases.append(float(elements[-1].split("(b)")[1]))
     return (np.asarray(network["weights"], dtype=object), 
             np.asarray(network["biases"], dtype=object))
 
+def file_reader(file):
+    return str_reader(file.read())
 
-def file_formatter(file):
-    return str_formatter(file.read())
+def str_writer(weights, biases) -> str:
+    string = ""
+    for weight, bias in zip(weights, biases):
+        for w, b in zip(weight, bias):
+            ws = ";".join([str(x) for x in w])
+            string += f"{ws};(b){b};\n"
+        string += "\n"
+    return string
+
+
+
+#liam berg var here
