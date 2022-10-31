01cfea17880bcadf9e0be78edeff8425ba041132
Casper Bené <48201144+cappe04@users.noreply.github.com>
2022-11-01 00:03:19 +0100

    changed file structure

--- sample/model_file_formatter.py
+++ sample/model_file_formatter.py
@@ -1,37 +1,32 @@
 import numpy as np
 
 def str_reader(string: str):
-    network = {
-        "weights": [],
-        "biases": []
-    }
     weights = []
     biases = []
-    for line in string.split("\n"):
-        if line == "":
-            network["weights"].append(weights)
-            network["biases"].append(biases)
-            weights = []
-            biases = []
-            continue
-        elements = line.split(";")[:-1]
-        weights.append([float(w) for w in elements[:-1]])
-        biases.append(float(elements[-1].split("(b)")[1]))
-    return (np.asarray(network["weights"], dtype=object), 
-            np.asarray(network["biases"], dtype=object))
+    for layer in string.split("\n\n")[:-1]:
+        lines = layer.split("\n")
+        weights.append(np.array([np.array(np.array(list(map(float, w)))) for w in [weight.split(";")[:-1] for weight in lines[:-1]]]))
+        biases.append(np.array([float(bias) for bias in lines[-1:][0].split(";")[:-1]]))
+    return (np.array(weights, dtype=object),
+            np.array(biases, dtype=object))
 
 def file_reader(file):
     return str_reader(file.read())
 
 def str_writer(weights, biases) -> str:
     string = ""
+    join = lambda s: ";".join([str(x) for x in s]) + ";"
     for weight, bias in zip(weights, biases):
-        for w, b in zip(weight, bias):
-            ws = ";".join([str(x) for x in w])
-            string += f"{ws};(b){b};\n"
-        string += "\n"
+        for w in weight:
+            string += f"{join(w)}\n"
+        string += f"{join(bias)}\n\n"
     return string
 
-
-
-#liam berg var here
+if __name__ == "__main__":
+    import os
+    path = os.path.join(os.path.dirname( __file__ ), "..", "data\\models\\example_file.wnb")
+    with open(path, "r") as file:
+        w, b = file_reader(file)
+        print(w[0])
+    # with open(path, "w") as file:
+    #     file.write(str_writer(w, b))
--- sample/network.py
+++ sample/network.py
@@ -16,12 +16,12 @@ class Network:
         """
         new_wnb = lambda shape: np.random.uniform(low=0, high=1, size=shape)
         weights, biases = self.model.load_wnb()
-        if weights is None:
-            for i, nodes in enumerate(self.model.structure[:-1]):
-                if weights is None:
-                    yield (new_wnb((nodes, self.model.structure[i+1])), new_wnb(nodes))
-                else:
-                    yield weights[i], biases[i]
+        for i, nodes in enumerate(self.model.structure[:-1]):
+            if weights is None:
+                yield (new_wnb((nodes, self.model.structure[i+1])), 
+                       new_wnb(self.model.structure[i+1])) 
+            else:
+                yield (weights[i], biases[i])
 
     def execute(self, inputs: np.ndarray) -> np.ndarray:
         for layer in self.layers:
@@ -34,18 +34,36 @@ class Network:
         return error * error
 
     def loss(self, data_in: np.ndarray, data_out: np.ndarray) -> float:
+        """
+        Find the total network loss for one "image".
+        data_in: np.ndarray should be a flattend array containing the image data
+        data_out: np.ndarray should be an array of zeros, conatining only one "1" at the spot
+        corresponding with it's label acording to the dataset mapping.
+
+        returns float
+        """
         layer_loss = map(self.__node_loss, self.execute(data_in), data_out)
         return sum(layer_loss)
+
+    def train(self, data: list[tuple[np.ndarray, np.ndarray]]):
+        """
+        Tweaks the weight and bias values in the network\n
+        data: should be a list of tuples containing:\n
+            1. a flatten np.ndarray of image data
+            2. a np.ndarray of zeros, containng one "1", representing the label after the datasets mapping
+        """
+
+
 
 if __name__ == "__main__":
     network = Network(model.Model.test_model)
     dataset = data.CompiledDataset("emnist-letters.mat", validation_partition=True, 
                                    as_array=True, flatten=True)
     costs = [network.loss(*_data) for _data in dataset.next_batch(10)]
-    print(costs)
+    print(f"COST: {sum(costs)/10}")
 
-    # weights, biases = [], []
-    # for layer in network.layers:
-    #     weights.append(layer.weights)
-    #     biases.append(layer.bias)
-    # network.model.save_wnb(weights, biases)
+    weights, biases = [], []
+    for layer in network.layers:
+        weights.append(layer.weights)
+        biases.append(layer.bias)
+    network.model.save_wnb(weights, biases)
