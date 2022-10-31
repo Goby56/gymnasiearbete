369a11ef48837eccb25c2156f52d9309dbe20038
Casper Bené <48201144+cappe04@users.noreply.github.com>
2022-10-31 18:51:05 +0100

    Moved creation of wnb to network

--- sample/layer.py
+++ sample/layer.py
@@ -3,16 +3,12 @@ from typing import Callable
 
 import model
 
-def _new_wnb(*shape: int) -> np.ndarray:
-    return np.random.uniform(low=0, high=1, size=shape)
-
 class Layer:
-    def __init__(self, in_nodes: int, out_nodes: int, 
-                 weights: np.ndarray, biases: np.ndarray):
-        self.weights = _new_wnb(in_nodes, out_nodes) if weights is None else weights
-        self.bias = _new_wnb(in_nodes) if biases is None else biases
+    def __init__(self, weights: np.ndarray, biases: np.ndarray):
+        self.weights = weights
+        self.bias = biases
 
-    def execute(self, inputs: np.ndarray, activation_func: model.Activation):
+    def execute(self, inputs: np.ndarray, activation_func: model.Activation) -> np.ndarray:
         out_values = []
         for node, node_weight, node_bias in zip(inputs, self.weights, self.bias):
             out_node = activation_func(sum(node*node_weight) + node_bias)
@@ -21,10 +17,4 @@ class Layer:
 
 
 if __name__ == "__main__":
-    w = _new_wnb(2, 3)
-    b = _new_wnb(2)
-    layer = Layer(2, 3, w, b)
-    out1 = layer.execute(np.array([2, 2]), model.Activation.ReLU)
-    out2 = layer.execute(np.array([2, 2]), model.Activation.Sigmoid)
-    print(out1)
-    print(out2)
+    print("haha")
