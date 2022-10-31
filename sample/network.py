cac8b2299209e557a3cc827e6e1442ae6343c79b
Casper Bené <48201144+cappe04@users.noreply.github.com>
2022-10-31 18:51:52 +0100

    added loss functions

--- sample/network.py
+++ sample/network.py
@@ -1,20 +1,51 @@
 import numpy as np
 
-import layer
-import model
-from typing import Callable
+import layer, model, data
 
 class Network:
     def __init__(self, model: model.Model):
         self.model = model
         self.layers = []
-        w, b = self.model.load_wnb()
-        for i in range(len(self.model.structure)-1):
-            layer.append(layer.Layer(self.model.structure[i], 
-                                     self.model.structure[i+1], 
-                                     w, b))
+        for w, b in self.__load_wnb():
+            self.layers.append(layer.Layer(w, b))
 
-    def execute(self, inputs: np.ndarray):
+    def __load_wnb(self):
+        """
+        Loads model .wnb file 
+        Creates wnb for network (not file) if .wnb is missing
+        """
+        new_wnb = lambda shape: np.random.uniform(low=0, high=1, size=shape)
+        weights, biases = self.model.load_wnb()
+        if weights is None:
+            for i, nodes in enumerate(self.model.structure[:-1]):
+                if weights is None:
+                    yield (new_wnb((nodes, self.model.structure[i+1])), new_wnb(nodes))
+                else:
+                    yield weights[i], biases[i]
+
+    def execute(self, inputs: np.ndarray) -> np.ndarray:
         for layer in self.layers:
-            inputs = layer.execute(inputs)
+            inputs = layer.execute(inputs, self.model.activation_func)
         return inputs
+
+    @staticmethod
+    def __node_loss(out: float, expected: float) -> float:
+        error = out - expected
+        return error * error
+
+    def loss(self, data_in: np.ndarray, data_out: np.ndarray) -> float:
+        layer_loss = map(self.__node_loss, self.execute(data_in), data_out)
+        return sum(layer_loss)
+
+if __name__ == "__main__":
+    network = Network(model.Model.test_model)
+    dataset = data.CompiledDataset("emnist-letters.mat", validation_partition=True, 
+                                   as_array=True, flatten=True)
+    costs = [network.loss(*_data) for _data in dataset.next_batch(10)]
+    print(costs)
+
+    # weights, biases = [], []
+    # for layer in network.layers:
+    #     weights.append(layer.weights)
+    #     biases.append(layer.bias)
+    # network.model.save_wnb(weights, biases)
