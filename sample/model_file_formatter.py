d6b2ae9eba43d216dd30420d03ee6283f754a077
Casper Bené <48201144+cappe04@users.noreply.github.com>
2022-11-01 01:42:35 +0100

    small bad shit

--- sample/model_file_formatter.py
+++ sample/model_file_formatter.py
@@ -6,7 +6,7 @@ def str_reader(string: str):
     for layer in string.split("\n\n")[:-1]:
         lines = layer.split("\n")
         weights.append(np.array([np.array(np.array(list(map(float, w)))) for w in [weight.split(";")[:-1] for weight in lines[:-1]]]))
-        biases.append(np.array([float(bias) for bias in lines[-1:][0].split(";")[:-1]]))
+        biases.append(np.array([float(bias) for bias in lines[-1].split(";")[:-1]]))
     return (np.array(weights, dtype=object),
             np.array(biases, dtype=object))
 
@@ -27,6 +27,6 @@ if __name__ == "__main__":
     path = os.path.join(os.path.dirname( __file__ ), "..", "data\\models\\example_file.wnb")
     with open(path, "r") as file:
         w, b = file_reader(file)
-        print(w[0])
+        #print(w[0])
     # with open(path, "w") as file:
     #     file.write(str_writer(w, b))
