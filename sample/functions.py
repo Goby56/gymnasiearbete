ae483d2b56a79ee27217d1836356019e2c130e1c
Casper Bené <48201144+cappe04@users.noreply.github.com>
2022-11-30 18:50:32 +0100

    Created functions.py + Added updated Activation enum

---
+++ sample/functions.py
@@ -0,0 +1,28 @@
+from enum import Enum
+from typing import Callable
+
+import math
+
+
+class Activation(Enum):
+    ReLU = (lambda x: max(0, x), lambda x: x > 0)
+    Sigmoid = (lambda x: 1 / (1 + math.exp(-x)),
+               lambda x: (1 / (1 + math.exp(-x))) * 1-(1 / (1 + math.exp(-x))))
+    Step = (lambda x: x > 0, lambda x: 0)
+
+    SiLU = lambda x: x / (1 + math.exp(-x)) # deprecated for now
+
+    def __init__(self, func: Callable, derivative: Callable):
+        self.__f = func
+        self.__df = derivative
+
+    @property
+    def f(self, __x: float) -> float:
+        return float(self.__f(__x))
+
+    @property
+    def df(self, __x: float) -> float:
+        return float(self.__df(__x))
+
+    def __call__(self, __x: float) -> float:
+        return float(self.__f(__x))
