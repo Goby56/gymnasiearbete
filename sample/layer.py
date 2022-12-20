9762a901b7ddd541b3988187bc2ed13daeb3ff42
Casper Bené <48201144+cappe04@users.noreply.github.com>
2022-12-20 23:25:24 +0100

    added support for matrix

--- sample/layer.py
+++ sample/layer.py
@@ -8,22 +8,31 @@ class Layer:
         self.weights = weights
         self.bias = biases
         self.in_nodes, self.out_nodes = weights.shape
-        self.inputs = np.empty(self.out_nodes, dtype=float)
+
+        self.dweights = np.zeros(weights.shape)
+        self.dbias = np.zeros(biases.shape)
 
     def execute(self, inputs: np.ndarray, activation_func: model.Activation) -> np.ndarray:
         """
         forward pass through the network
         """
-        self.inputs = np.dot(inputs, self.weights) + self.bias # Fig. 2
-        for i, v in enumerate(self.inputs):
-             self.inputs[i] = activation_func(v)
-        return self.inputs
+        self.inputs = inputs
+        output = np.dot(inputs, self.weights) + self.bias # Fig. 2
+        with np.nditer(output, op_flags=["readwrite"]) as it: # Iterate and modify all elements
+            for value in it: value[...] = activation_func(value)
+        return output
 
     def back(self, dvalues: np.ndarray, learn_rate: float) -> np.ndarray:
         """
         backward pass through the network
         """
-        out = np.dot(dvalues, self.weights.T)
-        self.bias -= learn_rate * np.sum(dvalues, keepdims=True)
-        self.weights -= learn_rate * np.dot(self.inputs, dvalues.T)
-        return out
+        self.dweights -= learn_rate * np.dot(self.inputs.T, dvalues)
+        self.dbias -= learn_rate * np.sum(dvalues, axis=0)
+        return np.dot(dvalues, self.weights.T)
+
+    def apply_trainings(self) -> None:
+        self.weights = self.dweights
+        self.bias = self.dbias
+
+if __name__ == "__main__":
+    print("Du kör fel fil din dummer! :(")
--- sample/network.py
+++ sample/network.py
@@ -1,6 +1,6 @@
 import numpy as np
 from typing import Generator, Iterable
-
+from functions import Accuracy
 import layer, model, data
 
 class Network:
@@ -40,42 +40,49 @@ class Network:
         confidence_scores = exp_out / layer_sum
         return confidence_scores
 
-    def run_batch(self, data: Iterable) -> float:
-        batch_outputs = []
-        batch_targets = []
-        for sample in data:
-            batch_outputs.append(self.execute(sample[0]))
-            batch_targets.append(sample[1])
-        return np.array(batch_outputs), np.array(batch_targets)
+    def run_batch(self, data: Iterable) -> tuple:
+        # TODO: fix so this is a nice one line np trick
+        samples, labels = [], []
+        for sample, label in data:
+            samples.append(sample)
+            labels.append(label)
+        samples, labels = np.array(samples), np.array(labels)
+        return self.execute(samples), labels
 
     def train(self, data: Iterable):
         batch_outputs, batch_targets = self.run_batch(data)
         batch_loss = self.model.loss_func.forward(batch_outputs, batch_targets)
+        batch_acc = Accuracy.calc(batch_outputs, batch_targets)
 
         # TODO fix batch backprop problem (matrix is being inputed instead of vector)
-        batches = self.model.loss_func.backward(batch_outputs, batch_targets)# Loss backpropagation
-        backprop_input = batches[0]
-        for batch in batches:
-            backprop_input += batch
-        backprop_input /= 10
+        bp_value = self.model.loss_func.backward(batch_outputs, batch_targets)# Loss backpropagation
         
-        print("Loss:", batch_loss)
+        print(f"Loss: {batch_loss:.2f}, Accuracy: {batch_acc:.2f}")
 
         for layer in self.layers[::-1]:
-            for i, v in enumerate(backprop_input.copy()):
-                backprop_input[i] = self.model.activation_func.df(v) * v
-            backprop_input = layer.back(backprop_input, self.model.learn_rate)
-            # print(layer.weights)
+            with np.nditer(bp_value.copy(), op_flags=["readwrite"]) as it: # Iterate and modify all elements
+                for v in it: v[...] = self.model.activation_func.df(v) * v # Derivative
+            bp_value = layer.back(bp_value, self.model.learn_rate)
+             
+        for layer in self.layers:
+            layer.apply_trainings()
 
 
 if __name__ == "__main__":
     network = Network(model.Model.test_model)
     dataset = data.CompiledDataset("emnist-letters.mat", validation_partition=True, 
                                    as_array=True, flatten=True, normalize=True)
-    print("-------------- Training ----------------")
+    
+    # tdata = dataset.next_batch(10)
+    # results, labels = network.run_batch(tdata)
+    # pairs = zip(results, labels)
+    # print(list(pairs)[0])
+
+    # print("-------------- Training ----------------")
     for _ in range(1000):
         network.train(dataset.next_batch(10))
-    tdata = next(dataset.next_batch(1))
-    print(network.execute(tdata[0]), tdata[1])
+
+    # delta = network.execute(tdata[0]) - result
+    # print(delta)
 
     # network.save_wnb()
