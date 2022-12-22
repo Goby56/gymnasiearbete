46883ef0250476b97fda73fc6864a7f1909d97b2
Casper Bené <48201144+cappe04@users.noreply.github.com>
2022-12-22 14:23:12 +0100

    fixed run_batch

--- sample/network.py
+++ sample/network.py
@@ -1,7 +1,7 @@
 import numpy as np
 from typing import Generator, Iterable
 from functions import Accuracy
-import layer, model, data
+import layer, model, data, logger
 
 class Network:
     def __init__(self, model: model.Model):
@@ -34,6 +34,7 @@ class Network:
     def execute(self, inputs: np.ndarray) -> np.ndarray:
         for layer in self.layers:
             inputs = layer.execute(inputs, self.model.activation_func)
+
         # Normalization using Soft-Max function
         exp_out = np.exp(inputs)
         layer_sum = np.sum(exp_out)
@@ -41,48 +42,42 @@ class Network:
         return confidence_scores
 
     def run_batch(self, data: Iterable) -> tuple:
-        # TODO: fix so this is a nice one line np trick
-        samples, labels = [], []
-        for sample, label in data:
-            samples.append(sample)
-            labels.append(label)
-        samples, labels = np.array(samples), np.array(labels)
+        samples, labels = map(np.asarray, zip(*data))
         return self.execute(samples), labels
 
     def train(self, data: Iterable):
         batch_outputs, batch_targets = self.run_batch(data)
-        batch_loss = self.model.loss_func.forward(batch_outputs, batch_targets)
-        batch_acc = Accuracy.calc(batch_outputs, batch_targets)
+        #batch_loss = self.model.loss_func.forward(batch_outputs, batch_targets)
+        #batch_acc = Accuracy.calc(batch_outputs, batch_targets)
 
         # TODO fix batch backprop problem (matrix is being inputed instead of vector)
         bp_value = self.model.loss_func.backward(batch_outputs, batch_targets)# Loss backpropagation
         
-        print(f"Loss: {batch_loss:.2f}, Accuracy: {batch_acc:.2f}")
+        # print(f"Loss: {batch_loss:.2f}, Accuracy: {batch_acc:.2f}")
 
         for layer in self.layers[::-1]:
-            with np.nditer(bp_value.copy(), op_flags=["readwrite"]) as it: # Iterate and modify all elements
+            with np.nditer(bp_value, op_flags=["readwrite"]) as it: # Iterate and modify all elements
                 for v in it: v[...] = self.model.activation_func.df(v) * v # Derivative
             bp_value = layer.back(bp_value, self.model.learn_rate)
              
         for layer in self.layers:
             layer.apply_trainings()
 
+    def _debug_get_weights(self):
+        return np.array([layer.weights for layer in self.layers])
+
 
 if __name__ == "__main__":
     network = Network(model.Model.test_model)
     dataset = data.CompiledDataset("emnist-letters.mat", validation_partition=True, 
                                    as_array=True, flatten=True, normalize=True)
+
+    start_weights = network._debug_get_weights()
+    # -------- training -----------
+    it = list(dataset.next_batch(10))
+    network.train(it)
     
-    # tdata = dataset.next_batch(10)
-    # results, labels = network.run_batch(tdata)
-    # pairs = zip(results, labels)
-    # print(list(pairs)[0])
+    delta = network._debug_get_weights() - start_weights
 
-    # print("-------------- Training ----------------")
-    for _ in range(1000):
-        network.train(dataset.next_batch(10))
-
-    # delta = network.execute(tdata[0]) - result
-    # print(delta)
-
+    logger.print_post()
     # network.save_wnb()
