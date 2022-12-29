76d60031dc34f23611daccc9a74ce464ba7bcdf2
Casper Bené <48201144+cappe04@users.noreply.github.com>
2022-12-29 19:37:21 +0100

    model revamp

--- sample/model.py
+++ sample/model.py
@@ -1,48 +1,84 @@
-from enum import Enum
-import os
-
-from functions import Activation, Loss, Loss_CCE
+import os, json
 from model_file_formatter import file_reader, str_writer
-
+import functions
 
 _MODEL_FOLDER = os.path.join(os.path.dirname( __file__ ), "..", "data\\models")
 
-class Model(Enum):
-    model1 = (Activation.ReLU, Loss_CCE, 1, (2, 3, 2))
-    test_model = (Activation.Sigmoid, Loss_CCE, 0.3, (28*28, 64, 26))
+class MissingConfigAttribute(Exception):
+    """Expetion raised when config.json is missing required options."""
 
-    def __init__(self, activation_func: Activation, loss_func: Loss, 
-                 learn_rate: int, structure: tuple[int]):
-        self.activation_func = activation_func
-        self.loss_func = loss_func
-        self.learn_rate = learn_rate
-        self.structure = structure
+    def __init__(self, attr):
+        self.attr = attr
+        super().__init__(f"config.json is missing required attribute '{attr}'")
 
-        self.__path = os.path.join(_MODEL_FOLDER, self._name_ + ".wnb")
-        self.have_wnb = os.path.exists(self.__path)
+class Model(dict):
+
+    function_table = {
+        "activation_function": "Activation_",
+        "loss_function": "Loss_",
+        "accuracy_function": "Accuracy_"
+    }
+
+    def __init__(self, name: str):
+        self.name = name
+
+        __dir_path = os.path.join(_MODEL_FOLDER, self.name)
+        self.paths = {
+            "dir": __dir_path,
+            "config": os.path.join(__dir_path, "config.json"),
+            "wnb": os.path.join(__dir_path, self.name + ".wnb"),
+        }
+
+        self.has_wnb = os.path.exists(self.paths["wnb"])
+
+        with open(self.paths["config"]) as file:
+            config = json.load(file)
+            setup = config["setup"]
+            training = config["training"]
+            
+        for key in {*setup.keys()} & {*training.keys()}:
+            assert setup[key] == training[key]
+
+        super().__init__(setup | training)
+
+        self.shape = self.structure[0], self.structure[-1]
+
+        # init funtion instances
+        def get(var):
+            function = getattr(functions, var, None)
+            if function is None:
+                raise Exception(f'function "{function}" does not exist!')
+            return function()
+        
+        for func in Model.function_table:
+            self[func] = get(Model.function_table[func] + self[func])
+
+    def __getattr__(self, attr):
+        return self[attr]
+
+    def __missing__(self, attr):
+        raise MissingConfigAttribute(attr)
 
     def load_wnb(self):
         """
-        Loads weights and bias from file {enum field name}.wnb
+        Loads weights and bias from file {model name}.wnb
+        returns (ndarray, ndarray)
         """
-        if not self.have_wnb: return None, None
-        with open(self.__path) as file:
+        with open(self.paths["wnb"]) as file:
             return file_reader(file)
 
     def save_wnb(self, weights, biases):
         """
-        Saves weights and bias to file {enum field name}.wnb
+        Saves weights and bias to file {model name}.wnb
         Creates file if it doesn't exist
 
         weights: an array or list of each layers weights
         biases: an array or list of each layers biases
         """
-        with open(self.__path, "w") as file:
+        with open(self.paths["wnb"], "w") as file:
             formatted_str = str_writer(weights, biases)
             file.write(formatted_str)
 
 
 if __name__ == "__main__":
-    if Model.model1.have_wnb:
-        weights, biases = Model.test_model.load_wnb()
-        print(f"weights: {weights[0]}, biases: {biases[0]}")
+    model = Model("test_model")
