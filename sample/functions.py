332c9c8bcb930129c4a2ad61ca4eb65826a63c74
Casper Bené <48201144+cappe04@users.noreply.github.com>
2023-02-16 16:12:20 +0100

    added ADAM, needs testing

--- sample/functions.py
+++ sample/functions.py
@@ -35,6 +35,7 @@ class Activation_ReLU:
         return inputs
 
 class Activation_ReLU6:
+    # INTE TESTAT
     def forward(self, inputs):
         self.inputs = inputs
         return np.maximum(0, np.minimum(inputs, 6))
@@ -46,21 +47,12 @@ class Activation_ReLU6:
 
 
 class Activation_Sigmoid:
-    # TODO: prob. fix 
-
     def forward(self, X):
         self.inputs = X
         return 1 / (1 + np.exp(-self.inputs))
 
     def backward(self, X):
         return self.inputs * self.forward(X) * (1- self.forward(X))
-
-class Activation_Step:
-    def f(self, x):
-        return x > 0
-
-    def df(self, x):
-        return 0
 
 # endregion Activation
 
@@ -141,7 +133,26 @@ class Accuracy_Categorical(Accuracy):
 
 # region Optimizer
 
-class Optimizer_SGD:
+class Optimzer:
+    """
+    A class all optimziers should enherit. Class must not overide all functions.
+    The functions are there to prevent error incase optimzer has no need for it.
+    All the functions are called in nework.backward().
+    ----------------------------------------------------------------------------
+    (pre_update) is called once per backpropagation before apply_traing. 
+    (apply_training) is called once per layer per backpropagation
+    (post_update) is called once per backpropagation after apply_traing.
+    """
+    def pre_update(self):
+        pass
+
+    def apply_training(self):
+        pass
+
+    def post_update(self):
+        pass
+
+class Optimizer_SGD(Optimzer):
     """
     Stochastic gradient decent - the most simple optimizer algorithm
     """
@@ -151,7 +162,7 @@ class Optimizer_SGD:
         self.decay = decay
         self.iterations = 0
 
-    def apply_decay(self):
+    def pre_update(self):
         if self.decay > 0:
             self.current_learn_rate = self.learn_rate * (1 / (1 + self.decay * self.iterations))
         self.iterations += 1
@@ -159,5 +170,59 @@ class Optimizer_SGD:
     def apply_training(self, layer):
         layer.weights -= self.learn_rate * layer.dweights
         layer.biases -= self.learn_rate * layer.dbiases
+
+
+class Optimzer_Adam(Optimzer):
+    # Behöver testas
+    def __init__(self, *, learn_rate, decay, epsilon, beta_1, beta_2):
+        self.learn_rate = learn_rate
+        self.current_learn_rate = learn_rate
+        self.decay = decay
+        self.epsilon = epsilon
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+
+        self.t = 0
+
+    def pre_update(self):
+        if self.decay:
+            self.current_learn_rate = self.learn_rate * (1 / (1 + self.decay * self.t))
+
+    def apply_training(self, layer):
+        """ Algorithm from 3.2.3 "Nätverkets inlärningsprocess" under rubriken "Vikter och biaser ändras" """
+
+        # init momentum vectors for weights and biases
+        # only called once per layer
+        if not hasattr(layer, 'v_dw'):
+            # 1st momentum vector
+            layer.v_dw = np.zeros_like(layer.weights) # samma som np.zeros(layer.weights.shape)
+            layer.v_db = np.zeros_like(layer.biases)
+            # 2nd momentum vector
+            layer.s_dw = np.zeros_like(layer.weights)
+            layer.s_db = np.zeros_like(layer.biases)
+        
+        # 1st momentum vector using beta_1. m_t = beta_1 * m_(t-1) + (1-beta_1) * g
+        layer.v_dw = self.beta_1 * layer.v_dw + (1 - self.beta_1) * layer.dweights
+        layer.v_db = self.beta_1 * layer.v_db + (1 - self.beta_1) * layer.dbiases
+        
+        # g^2 indicates elementwise square.
+        # 2nd momentum vector using beta_2. v_t = beta_2 * v_(t-1) + (1-beta_2) * g^2
+        layer.s_dw = self.beta_2 * layer.s_dw + (1 - self.beta_2) * layer.dweights**2 # samma som np.square
+        layer.s_db = self.beta_2 * layer.s_db + (1 - self.beta_2) * layer.dbiases**2 # samma som np.square
+
+        # 1st moment vector correction. m_t_hat = m_t / (1 - beta_1^t)
+        v_dw_corrected = layer.v_dw / (1 - self.beta_1 ** (self.t + 1)) # t + 1 efterson t börjar på 0
+        v_db_corrected = layer.v_db / (1 - self.beta_1 ** (self.t + 1))
+
+        # 2nd moment vector correction. v_t_hat = v_t / (1 - beta_2^t)
+        s_dw_corrected = layer.s_dw / (1 - self.beta_2 ** (self.t + 1))
+        s_db_corrected = layer.s_db / (1 - self.beta_2 ** (self.t + 1))
+        
+        # update params. theta_t = theta_(t-1) - alpha * m_t_hat / (sqrt(v_t_hat) + epsilon)
+        layer.weights -= self.current_learn_rate * v_dw_corrected / (np.sqrt(s_dw_corrected) + self.epsilon)
+        layer.biases -= self.current_learn_rate * v_db_corrected / (np.sqrt(s_db_corrected) + self.epsilon)
+
+    def post_update(self):
+        self.t += 1
 
 # endregion Optimizer
