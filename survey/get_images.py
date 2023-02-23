044b412911dc101bd9e3894e55aabc36a7dadca9
Casper Bené <48201144+cappe04@users.noreply.github.com>
2023-02-23 22:47:58 +0100

    Create get_images.py

---
+++ survey/get_images.py
@@ -0,0 +1,17 @@
+import os, sys
+from PIL import Image
+
+sys.path.append(os.getcwd())
+
+from sample import CompiledDataset
+
+dataset = CompiledDataset(
+    filename="emnist-letters.mat", 
+    image_size=(28, 28)
+)
+
+for _ in dataset.next_batch(69):
+    pass
+
+for image, label in dataset.get(25, convert=True):
+    Image.fromarray(image).convert("L").save(f"survey/emnist-{label}.png")
