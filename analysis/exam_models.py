9f2b82693d5cdf952a248593d0f20f7ded7f454a
Casper Bené <48201144+cappe04@users.noreply.github.com>
2023-03-07 20:53:10 +0100

    more tests

--- analysis/exam_models.py
+++ analysis/exam_models.py
@@ -9,32 +9,39 @@ import sample
 PATH_EXTRA_IMAGES = os.path.join(os.getcwd(), "survey\\extra_images")
 PATH_IMAGES = os.path.join(os.getcwd(), "survey\\images")
 
-model = sample.Model(input("AI name: "))
-network = sample.Network(model)
+while True:
+    model = sample.Model(input("AI name: "))
+    network = sample.Network(model)
+    dataset = sample.CompiledDataset(
+        filename=model.dataset
+    )
 
-res_cache = [0, 0] # num failed, num sucsess
+    res_cache = [0, 0] # num failed, num sucsess
 
-def guess(image, ans):
-    array = np.asarray(image)
-    array.shape = (1, array.size)
+    def guess(image, ans):
+        array = np.asarray(image)
+        array.shape = (1, array.size)
 
-    out_vec = network.forward(array)
-    i = np.where(out_vec == out_vec.max())[1][0]
-    res_cache[ans == model.mapping[i]] += 1
+        out_vec = network.forward(array)
+        i = np.where(out_vec == out_vec.max())[1][0]
+        res_cache[ans == model.mapping[i]] += 1
 
-def get_ans(filename):
-    return re.search("(?<=_).", filename)[0]
+    def get_ans(filename):
+        return re.search("(?<=_).", filename)[0]
 
-images = [(os.path.join(PATH_IMAGES, filename), get_ans(filename)) 
-          for filename in os.listdir(PATH_IMAGES)]
-extra_images = [(os.path.join(PATH_EXTRA_IMAGES, filename), 
-                 get_ans(filename)) for filename in os.listdir(PATH_EXTRA_IMAGES)]
+    images = [(os.path.join(PATH_IMAGES, filename), get_ans(filename)) 
+            for filename in os.listdir(PATH_IMAGES) if not "emnist" in filename]
+    extra_images = [(os.path.join(PATH_EXTRA_IMAGES, filename), 
+                    get_ans(filename)) for filename in os.listdir(PATH_EXTRA_IMAGES)]
 
-for path, key in images + extra_images:
-    image = Image.open(path).convert("L")
-    guess(image, key)
+    for path, key in images + extra_images:
+        image = Image.open(path).convert("L")
+        guess(image, key)
 
-print(res_cache)
+    for data in dataset.get(150, convert=True, target="test"):
+        guess(*data)
+
+    print(res_cache, res_cache[1]/sum(res_cache))
 
 
 
