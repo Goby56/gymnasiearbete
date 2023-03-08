bae75e881ec4f58d12581fa21163ad6828d858ed
Goby56 <60710855+Goby56@users.noreply.github.com>
2023-03-08 10:27:40 +0100

    better exam

--- analysis/exam_models.py
+++ analysis/exam_models.py
@@ -7,12 +7,20 @@ sys.path.append(os.getcwd())
 import sample
 
 PATH_EXTRA_IMAGES = os.path.join(os.getcwd(), "survey\\extra_images")
-PATH_IMAGES = os.path.join(os.getcwd(), "survey\\images")
+PATH_SURVEY_IMAGES = os.path.join(os.getcwd(), "survey\\images")
+PATH_BALANCED_IMAGES = os.path.join(os.getcwd(), "analysis\\balanced_images")
+PATH_DIGIT_IMAGES = os.path.join(os.getcwd(), "analysis\\digit_images")
 
-PATH_DIGITS = os.path.join(os.getcwd(), "analysis\\digit_images")
+print("Image sets available: 300 | 200 | 100")
+# 300 is used for model choosing, 200 is for comparison (AI vs Human)
+# 100 is digits used for specialized vs general 
 
 while True:
-    model = sample.Model(input("AI name: "))
+    model_name, image_set = input("Model name & image set -> ").split(" ")
+    if image_set not in ["300", "200", "100"]:
+        print(f"Not an available image set: {image_set}")
+        continue
+    model = sample.Model(model_name)
     network = sample.Network(model)
     dataset = sample.CompiledDataset(
         filename=model.dataset
@@ -30,22 +38,23 @@ while True:
 
     def get_ans(filename):
         return re.search("(?<=_).", filename)[0]
-
-    images = [(os.path.join(PATH_IMAGES, filename), get_ans(filename)) 
-            for filename in os.listdir(PATH_IMAGES)]# if not "emnist" in filename]
-    extra_images = [(os.path.join(PATH_EXTRA_IMAGES, filename), 
-                    get_ans(filename)) for filename in os.listdir(PATH_EXTRA_IMAGES)]
     
-    # images = [(os.path.join(PATH_DIGITS, filename), 
-    #                 get_ans(filename)) for filename in os.listdir(PATH_DIGITS)]
-
-    for path, key in images:# + extra_images:
+    if image_set == "300":
+        images = [(os.path.join(PATH_SURVEY_IMAGES, filename), get_ans(filename)) 
+                for filename in os.listdir(PATH_SURVEY_IMAGES) if not "emnist" in filename]
+        images += [(os.path.join(PATH_EXTRA_IMAGES, filename), get_ans(filename)) 
+                   for filename in os.listdir(PATH_EXTRA_IMAGES)]
+        images += [(os.path.join(PATH_BALANCED_IMAGES, filename), get_ans(filename)) 
+                   for filename in os.listdir(PATH_BALANCED_IMAGES)]
+    elif image_set == "200":
+        images = [(os.path.join(PATH_SURVEY_IMAGES, filename), get_ans(filename)) 
+                for filename in os.listdir(PATH_SURVEY_IMAGES)]
+    elif image_set == "100":
+        images = [(os.path.join(PATH_DIGIT_IMAGES, filename), get_ans(filename)) 
+                  for filename in os.listdir(PATH_DIGIT_IMAGES)]
+        
+    for path, key in images:
         image = Image.open(path).convert("L")
         guess(image, key)
 
-    # for data in dataset.get(150, convert=True, target="test"):
-    #     guess(*data)
-
     print(res_cache, res_cache[1]/sum(res_cache))
-
-    
--- analysis/get_emnist_digits.py
+++ analysis/get_emnist_digits.py
@@ -4,13 +4,13 @@ from PIL import Image
 sys.path.append(os.getcwd())
 from sample import CompiledDataset
 
-IMAGES_FOLDER = os.path.join(os.getcwd(), "analysis\\digit_images")
+IMAGES_FOLDER = os.path.join(os.getcwd(), "analysis\\balanced_images")
 
 dataset = CompiledDataset(
-    filename="emnist-digits.mat" 
+    filename="emnist-balanced.mat" 
 )
 
-for i, sample in enumerate(dataset.get(50, convert=True, target="test")):
+for i, sample in enumerate(dataset.get(150, convert=True, target="test")):
     Image.fromarray(sample[0]).convert("RGB").save(
         os.path.join(IMAGES_FOLDER, f"emnist_{sample[1]}({i}).png")
     )
